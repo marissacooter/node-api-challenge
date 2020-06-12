6c8eecbea8c5849e29b9828bee8d8dcf7739a7dc
Marissa <marissacooterl@gmail.com>
2020-06-12 18:51:22 -0500

    get projects done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const projectsRouter = require("./projects/projectsRouter")
 
 const server = express()
-const port = 6000
+const port = 7000
 
 server.use(express.json())
+server.use('/api/projects', projectsRouter)
 
 server.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -3,4 +3,20 @@ const projects = require("../data/helpers/projectModel")
 
 const router = express.Router()
 
-module.exports = router
+// GET
+router.get('/', (req, res) => {
+    const id = req.params.id
+    projects
+        .get(id)
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "Error retreiving projects."
+            })
+        })
+})
+
+module.exports = router;
