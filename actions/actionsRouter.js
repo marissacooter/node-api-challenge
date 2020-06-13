b88bd11ebdab56052147d4dcb6878e50118cd2ee
Marissa <marissacooterl@gmail.com>
2020-06-12 20:16:33 -0500

    delete action done

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -73,5 +73,23 @@ router.put('/:id', (req, res) => {
         })
 })
 
+// DELETE
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    actions
+        .remove(id)
+        .then((action) => {
+            res.status(200).json({
+                message: "The action has been deleted."
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "Error deleting action."
+            })
+        })
+})
+
 
 module.exports = router 
