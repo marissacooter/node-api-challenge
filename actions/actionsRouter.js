f55c1633a8c2c750dd7706f0b17632f275a5c21c
Marissa <marissacooterl@gmail.com>
2020-06-12 19:43:57 -0500

    get actions done

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -3,5 +3,21 @@ const actions = require("../data/helpers/actionModel")
 
 const router = express.Router()
 
+// GET 
+router.get('/', (req, res) => {
+    const id = req.params.id
+    actions
+        .get(id)
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "Error retrieving actions."
+            })
+        })
+})
+
 
 module.exports = router 
