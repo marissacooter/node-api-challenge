92548069e0487367b968a7ee417460f4070b3a16
Marissa <marissacooterl@gmail.com>
2020-06-12 19:39:38 -0500

    actions router stubbed out

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const projectsRouter = require("./projects/projectsRouter")
+const actionsRouter = require("./actions/actionsRouter")
 
 const server = express()
 const port = 7000
 
 server.use(express.json())
 server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
 
 server.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
